0b4055c94337a0495ee3ad5ca782106c5829cd8d
TnJUFI <xtrgaming233@gmail.com>
2025-02-03 21:34:07 +0700

    orders-dashboard-route

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function OrdersPage() {
     return (
@@ -19,6 +19,18 @@ export default function OrdersPage() {
                             <TableHead className="text-right">Amount</TableHead>
                         </TableRow>
                     </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <p className="font-medium">Jul Famaji</p>
+                                <p className="hidden md:flex text-sm text-muted-foreground">aji@gmail.com</p>
+                                </TableCell>
+                                <TableCell>Sale</TableCell>
+                                <TableCell>Successfull</TableCell>
+                                <TableCell>01-02-2025</TableCell>
+                                <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                    </TableBody>
                 </Table>
             </CardContent>
         </Card>
