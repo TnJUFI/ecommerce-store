c0e9a8a899713a22acbd10a8b6da78cfd858e1dd
TnJUFI <xtrgaming233@gmail.com>
2025-02-04 23:40:30 +0700

    dashboard-product-route

--- app/components/dashboard/DashboardNavigation.tsx
+++ app/components/dashboard/DashboardNavigation.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
     {
@@ -11,7 +15,7 @@ const links = [
     },
     {
         name: "Products",
-        href: "/dashboard/product",
+        href: "/dashboard/products",
     },
     {
         name: "Categories",
@@ -20,10 +24,18 @@ const links = [
 ];
 
 export function DashboardNavigation() {
+    const pathname = usePathname()
     return (
         <>
         {links.map((link) => (
-            <Link key={link.href} href={link.href}>{link.name}</Link>
+            <Link
+                key={link.href}
+                href={link.href}
+                className={cn(link.href === pathname
+                ? "text-foreground"
+                : "text-muted-foreground hover:text-foreground")}
+                >{link.name}
+            </Link>
         ))}
         </>
     )
